90dd7ee0869216e21c36cfdce3bc0837d6b31308
oussema.sellami <49963438+oussemasellami@users.noreply.github.com>
2025-02-17 16:38:58 +0100

    service part

---
+++ src/app/app.module.ts
@@ -0,0 +1,44 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { ResidenceComponent } from './residence/residence.component';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import{RouterModule, Routes} from '@angular/router';
+import { NotfoundComponent } from './notfound/notfound.component';
+import { ApproutingModule } from './approuting.module';
+import { AnnonceComponent } from './annonce/annonce.component';
+import { ListannonceComponent } from './annonce/listannonce/listannonce.component';
+import { AnnonceRoutingModule } from './annonce/annonce-routing.module';
+import { AnnonceModule } from './annonce/annonce.module';
+import { FormResidenceComponent } from './form-residence/form-residence.component'
+import {HttpClientModule} from '@angular/common/http'
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    HeaderComponent,
+    FooterComponent,
+    ResidenceComponent,
+    NotfoundComponent,
+    FormResidenceComponent,
+  
+  
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    ApproutingModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    AnnonceModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/service/residence.service.ts
@@ -0,0 +1,40 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Residence } from 'src/core/models/residence';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ResidenceService {
+UrlResidence="http://localhost:3000/residences"
+  constructor(private http:HttpClient) { }
+
+  getnumberinlist(list:any,creteria:any,value:any){
+    let n=0
+    for(let i in list){
+
+      if(list[i][creteria]==value){
+        n++
+      }
+    }
+return n
+  }
+
+
+  getallResidence():Observable<Residence[]>{
+    return this.http.get<Residence[]>(this.UrlResidence)
+  }
+  addResidence(res:Residence):Observable<Residence[]>{
+    return this.http.post<Residence[]>(this.UrlResidence,res)
+  }
+  updateResidence(res:Residence,id:any):Observable<Residence[]>{
+    return this.http.put<Residence[]>(this.UrlResidence+'/'+id,res)
+  }
+  getResidence(id:any):Observable<Residence[]>{
+    return this.http.get<Residence[]>(this.UrlResidence+'/'+id)
+  }
+  deleteResidence(id:any):Observable<Residence[]>{
+    return this.http.delete<Residence[]>(this.UrlResidence+'/'+id)
+  }
+}
